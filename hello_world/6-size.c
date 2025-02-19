276964c8c5285ac6e089b8a6338ef5cc23218101
HISvessel <kevinsanchezdabest@yahoo.com>
2025-02-19 15:58:18 -0400

    Sizing up

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Main - Sizing up
+ * Return - 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu 1 byte(s)\n");
+	printf("Size of an int: %zu 4 byte(s)\n");
+	printf("Size of a long unit: %zu 4 byte(s)\n");
+	printf("Size of long long nit: %zu 8 byte(s)\n");
+	printf("Size of a float: %zu 4 byte(s)\n");
+
+	return (0)
+}
