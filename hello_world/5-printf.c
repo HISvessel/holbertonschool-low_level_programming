be33d3081b7589589bb81e939f5a4bdf9aec533f
HISvessel <kevinsanchezdabest@yahoo.com>
2025-02-19 14:04:53 -0400

    printing with printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Printing for printf
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piee of art,\n");
+	return (0);
+}
