e0b3d47dd4ad605db58a161d4804529f1a8511c3
HISvessel <kevinsanchezdabest@yahoo.com>
2025-02-19 12:22:41 -0400

    Hello

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-int main(void
-	/*
-	*This is the core of programming
-	 */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-		puts(Programming is like building a multilingual puzzle / n);
-		return (0);
-		}
+    /* This is the core of programming */
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
+
