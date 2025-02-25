e8f5104b4bf633ab4f8fc80d0ea0b359766ad1bc
HISvessel <kevinsanchezdabest@yahoo.com>
2025-02-25 13:23:54 -0400

    srettel lla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - tebahpla rof tniop yrtne
+*Return: 0
+*/
+int main(void)
+{
+	char l = 122;
+
+		for (l = 122; l >= 97; l--)
+		{
+			putchar(l);
+		}
+		{
+			putchar('\n');
+		}
+		return (0);
+}
+
