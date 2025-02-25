a61f908e58037623e1363d1fc60774ebfdbd07de
HISvessel <kevinsanchezdabest@yahoo.com>
2025-02-25 10:49:53 -0400

    No q or e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*Return: 0
+*/
+int main(void)
+{
+	char a = 97;
+
+	while (a <= 122)
+		if (a != 101 && 113)
+		{
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
