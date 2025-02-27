bf0b29a66e94c6206ecfbc4b0d64fcf60655acab
HISvessel <kevinsanchezdabest@yahoo.com>
2025-02-27 10:28:23 -0400

    printing m own alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - entry point for alphabet
+*Return: 0
+*/
+void print_alphabet(void)
+{
+	char (a) = 97;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar ('\n');
+
+}
