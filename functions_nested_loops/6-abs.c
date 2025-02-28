32619eb2f7cebcbf9857198f2caaefda2c7c468a
HISvessel <kevinsanchezdabest@yahoo.com>
2025-02-28 17:14:57 -0400

    learning to print the absolute value of a variable within another function

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - finding the absolute value.
+ *@n: integer for abolute value
+ *Return: Absolute value of an integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
