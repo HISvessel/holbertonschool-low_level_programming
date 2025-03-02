ee0ef44a6659e862f3d01ca8309a89a591908e34
HISvessel <kevinsanchezdabest@yahoo.com>
2025-03-02 17:48:19 -0400

    adding two

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds the value of two variables
+ *@a: variable to add
+ *@b: variable to add
+ *Return: sum of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+	return (a + b);
+}
