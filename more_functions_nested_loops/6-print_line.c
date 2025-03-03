4d996a568c947e90422b4a8302950c18b7bc59f3
HISvessel <kevinsanchezdabest@yahoo.com>
2025-03-03 11:48:04 -0400

    printing lines on a test subject, working with interchanged variables from one text to the next remembering that if a user called function has been given a named var, we must declare its parameters

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - this function is to print number of lines
+ *@n: number of lines to print
+ *Return: void
+ */
+void print_line(int n)
+{
+	int u;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (u = 0; u <= n; u++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
