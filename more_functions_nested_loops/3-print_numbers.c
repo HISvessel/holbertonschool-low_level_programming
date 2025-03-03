92eff38df3b16a4ca97cf807e4d20c8f37d6f3af
HISvessel <kevinsanchezdabest@yahoo.com>
2025-03-03 02:00:25 -0400

    printing all numbers while never forgetting to include all usable functions in our main header

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - printing a string of numbers
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
