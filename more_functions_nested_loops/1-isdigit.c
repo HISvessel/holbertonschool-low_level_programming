6f76405ef190cbe15ef8e39d48ccba57e577a4cc
HISvessel <kevinsanchezdabest@yahoo.com>
2025-03-02 22:30:13 -0400

    pritning if it is a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * isdigit - checks for a digit from 0 to 9
+ *@c: char variable
+ *Return: 0 to 1
+ */
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+		return (1);
+	else
+		return (0);
+}
