fa256a395d5383aa0509d5e6bde1bfd8ad871d54
HISvessel <kevinsanchezdabest@yahoo.com>
2025-03-04 12:43:56 -0400

    returning the size of a given string with strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - this function prints out the string length
+ *@s: char to measure
+ *Return: length
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+		return (len);
+
+}
