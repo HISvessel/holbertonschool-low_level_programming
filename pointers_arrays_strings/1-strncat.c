098a8c15f6fdf4039a62b70390b6e08e4053696d
HISvessel <kevinsanchezdabest@yahoo.com>
2025-03-09 23:38:36 -0400

    this function concatenates two strings together, the big distinction is that i can more specifically add up to any maximum number of bytes desired from my src string

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcat - this function catenates a string
+ * _strncat - this function catenates a string
  *@src: the string to append
  *@dest: the string which will catenate with another
  *@n: maximum number of bytes used from arc
@@ -12,15 +12,15 @@ char *_strncat(char *dest, char *src, int n)
 	int j = 0;
 
 	while (dest[i] != '\0')
-		{
-			i++;
-		}
+	{
+		i++;
+	}
 
-		for (j = 0; j < n && src[j] != '\0'; j++)
-			{
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
 		dest[i + j] = src[j];
-			}
-	dest[i +j] = '\0';
+	}
+	dest[i + j] = '\0';
 	return (dest);
 }
 
