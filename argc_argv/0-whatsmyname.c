8fdbda2d5445b1d93be858c43d2ddaa38906a68d
HISvessel <kevinsanchezdabest@yahoo.com>
2025-03-21 12:50:25 -0400

    this function prints the result of argument counters -1; when you play with the counters, you can use printf in a more flexible way by using arithmetics employed within the function

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,16 +2,12 @@
 #include <string.h>
 
 /**
- * main - printing the program name
- * @argc: argument number passed
- * @argv: array of arguments
+ * Main - printing the program name
  *Return: 0 upon success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	(void) argc;
 
-	printf("%s\n", argv[0]);
 	return (0);
 }
---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this function prints the number of arguments passed
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main (int argc, char *argv[])
+{
+	(void) argv;
+	
+		printf("%d\n", argc - 1);
+	return (0);
+}
