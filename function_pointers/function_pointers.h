a814a7abe2224f0afa2de2d01e0c5e7039fb4f2b
HISvessel <kevinsanchezdabest@yahoo.com>
2025-03-24 19:01:09 -0400

    putting include guards on our header files; these are meant to protect both the header files from double inclusion and our functions from several compilation errors

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,7 @@
+#ifndef "function_pointer.h"
+#define "function_pointer.h"
 void print_name(char *name, void(*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *aray, int size, int(*cmp)(int));
 int _putchar(char c);
+#endif
