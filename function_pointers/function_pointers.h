ac78986db0367117aae614ab797c41720a37e7f9
HISvessel <kevinsanchezdabest@yahoo.com>
2025-03-24 19:57:01 -0400

    pushing it again, this time with the proper header gards, otherwise it will not exist and will contain the header file as unexistent and therefore, reduce it to an implcit declaration. remember to guard it without using incorrect characters

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef "function_pointer.h"
-#define "function_pointer.h"
+#ifndef FUNCTION_POINTERS_C
+#define FUNCTION_POINTERS_C
 void print_name(char *name, void(*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *aray, int size, int(*cmp)(int));
