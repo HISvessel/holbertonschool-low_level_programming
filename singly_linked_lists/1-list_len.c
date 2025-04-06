3a9511de88044199797026af0dd12e8af061ab3a
HISvessel <kevinsanchezdabest@yahoo.com>
2025-04-06 18:39:46 -0400

    this function returns the size of our linked list, this is simple enough, no need to complicate things so much. Again, remember to set the return value correctly and use the return statement as we should be, and of course, iterating through both the nodes and through the counter variable

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * list_len - this function returns the amout of nodes in a linked list
+ * @h: pointer to a structurre
+ * Return: the amount of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+	const list_t *temp = h;
+
+	if (temp == NULL)
+	{
+		return (1);
+	}
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		counter++;
+	}
+		return (counter);
+}
