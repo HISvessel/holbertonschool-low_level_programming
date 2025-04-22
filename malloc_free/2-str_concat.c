2b95a0ea7ede23a164df3340de0a53822e631a79
HISvessel <kevinsanchezdabest@yahoo.com>
2025-04-21 20:56:14 -0400

    iterating through arrays and catenating strings

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - this function concatenates two strings together
+ * @s1: source string to append to
+ * @s2: dest string to append
+ * Return: the two strings together
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len_1 = 0, len_2 = 0, i;
+	char *concat;
+	
+	if (!s1 && !s2)
+	{
+		s1 = "";
+		s2 = "";
+	}
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		len_1++;
+		len_2++;
+	}
+
+	concat = malloc(sizeof(char) * (len_1 + len_2) + 1);
+
+	if (concat == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		concat[i] = s1[i];
+	for (i = 0; s2[i] != '\0'; i++)
+		concat[len_1 + i] = s2[i];
+	return (concat);
+}
---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - this function creates a 2d array
+ * @width: the amount of horizontally placed elements
+ * @height: the amount of vertically palced elements
+ * Return: int, else 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(int) * height);
+
+	if (!grid)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc (sizeof(int) * width);
+		if (!grid[i])
+			return (NULL);
+
+		for (j = 0; j < i; j++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				grid[i][j] = 0;
+			}
+		}
+	}
+	return (grid);
+}
