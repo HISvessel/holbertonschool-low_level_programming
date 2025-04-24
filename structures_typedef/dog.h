75af7b3c21274c76ded107e9041beb3af3aceba7
HISvessel <kevinsanchezdabest@yahoo.com>
2025-04-24 14:32:37 -0400

    checking out our mistake

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/* void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d); */
+
+#endif
