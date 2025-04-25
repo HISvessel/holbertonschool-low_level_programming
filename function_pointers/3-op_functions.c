eeb6c91accd93bff6560ee00259817cb9a34c954
HISvessel <kevinsanchezdabest@yahoo.com>
2025-04-25 08:51:23 -0400

    this is a calculator

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adding two numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: sum of operands
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substracting two numbers
+ * @a: frst operand
+ * @b: second operand
+ * Return: the difference of nums
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplying to numbers
+ * @a: the first operand
+ * @b: the second operand
+ * Return: the product of nums
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - dividing two numbers
+ * @a: the first operand
+ * @b: the second operand
+ * Return: the cuocient of nums
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulating two numbers
+ * @a: the first operand
+ * @b: the second operand
+ * Return: the percent of nums
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
