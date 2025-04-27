e3448f0add5aa5722b4790785b296540bd0acb1a
HISvessel <kevinsanchezdabest@yahoo.com>
2025-04-27 15:45:51 -0400

    this function traverses each node forward and returns the amount of nodes contained on a linked lists.

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - this function prints the number of nodes
+ * @h: the pointer to a node
+ * Return: the size of the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
